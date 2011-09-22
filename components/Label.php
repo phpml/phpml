84c7393f6a79dc0a2f1b1750bd68236fbba2a7aa
Leandro <lleitep3@gmail.com>
2011-09-22 12:48:18 +0000

    moving __toString to classe "Component" and add o "getTag" method at return tagName (is the same name of called class but lower case)

--- components/Label.php
+++ components/Label.php
@@ -7,12 +7,7 @@ class Label extends Component
     public function __construct()
     {
         parent::__construct();
-        
+
         $this->properties['value'] = null;
-    }
-    
-    public function __toString()
-    {
-        return "<p>{$this->properties['value']}</p>";
     }
 }
