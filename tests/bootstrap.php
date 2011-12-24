2db14d0d887744aec960c920efd77655bfc8d38f
Thiago Rigo <thiagophx@gmail.com>
2011-12-24 20:10:48 -0200

    Removing some configurations from phpunit.xml and improving bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,15 +1,27 @@
 <?php
 
-date_default_timezone_set('UTC');
 set_include_path(dirname(__DIR__) . DIRECTORY_SEPARATOR . 'lib' . PATH_SEPARATOR . get_include_path());
 
-spl_autoload_register(function($className) {
-    $fileParts = explode('\\', ltrim($className, '\\'));
+/**
+ * Autoloader that implements the PSR-0 spec for interoperability between
+ * PHP software.
+ * 
+ * Got it from github.com/Respect
+ */
+spl_autoload_register(
+    function($className) {
+        $fileParts = explode('\\', ltrim($className, '\\'));
 
-    if (false !== strpos(end($fileParts), '_'))
-        array_splice($fileParts, -1, 1, explode('_', current($fileParts)));
+        if (false !== strpos(end($fileParts), '_'))
+            array_splice($fileParts, -1, 1, explode('_', current($fileParts)));
 
-    require implode(DIRECTORY_SEPARATOR, $fileParts) . '.php';
-});
+        $file = implode(DIRECTORY_SEPARATOR, $fileParts) . '.php';
+
+        foreach (explode(PATH_SEPARATOR, get_include_path()) as $path) {
+            if (file_exists($path = $path . DIRECTORY_SEPARATOR . $file))
+                return require $path;
+        }
+    }
+);
 
 define('FILES_DIR', __DIR__ . DIRECTORY_SEPARATOR . '_files' . DIRECTORY_SEPARATOR);
